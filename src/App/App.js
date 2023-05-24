0bb8b9d0973d28c4b75f12e1c9f097076a799b63
LAPTOP-5DDU3O4T\rdchen <rdchen1124@gmail.com>
2023-05-24 23:51:47 +0800

    feat: 使用 styled 增加樣式, 把 Todo 輸入框內容綁定 state
    因應需求調整:
       Component 需要加入 CSS 樣式,
       Todo 項目的內容需要被記錄
    
    調整項目:
    1. 讓樣式與 Component 綁定在一起,
    2. 輸入時紀錄 Todo 的內容

--- src/App/App.js
+++ src/App/App.js
@@ -1,18 +1,47 @@
+import styled from "@emotion/styled";
+import { useState } from "react";
+
+const Container = styled.div`
+  background-color: #ededed;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const TodoListWrapper = styled.div`
+  position: relative;
+  min-width: 360px;
+  box-sizing: border-box;
+  padding: 30px 15px;
+  background-color: white;
+`;
 function App() {
+  const [value, setValue] = useState("");
+  const handleTodoChange = (todo) => {
+    setValue(todo);
+  };
   return (
-    <div>
-      <header>
-        <h1>Todo List in React</h1>
-      </header>
-      <section>
-        <input type="text" />
-        <input type="button" value="addTodo" />
-        {/* todo form */}
-      </section>
-      <section>{/* filter */}</section>
-      <hr />
-      <section>{/* todo items */}</section>
-    </div>
+    <Container>
+      <TodoListWrapper>
+        <header>
+          <h1>Todo List in React</h1>
+        </header>
+        <section>
+          <input
+            type="text"
+            onChange={(e) => {
+              handleTodoChange(e.target.value);
+            }}
+          />
+          <input type="button" value="addTodo" />
+          {/* todo form */}
+        </section>
+        <section>{/* filter */}</section>
+        <hr />
+        <section>{/* todo items */}</section>
+      </TodoListWrapper>
+    </Container>
   );
 }
 
