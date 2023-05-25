5f4b74b4ba2fedfc44b6cdcd73b281f14997bc6a
LAPTOP-5DDU3O4T\rdchen <rdchen1124@gmail.com>
2023-05-25 15:31:33 +0800

    feat: 增加刪除單個 Todo 功能
    因應需求調整:
       在每個 Todo 項目增加刪除按鈕, 點擊會將此 Todo 項目從 todoList 清除
    
    調整項目:
    1. 在 Todo 項目旁增加刪除按鈕, 並傳入對應 Todo 的 id 到 onClick 事件
    2. 撰寫 handleDeleteOne 方法, 將其綁定 刪除按鈕 的 onClick 事件

--- src/App/App.js
+++ src/App/App.js
@@ -37,6 +37,16 @@ function App() {
       id.current = 1;
     }
   };
+  const handleDeleteOne = (todoID) => {
+    if (todoList.length === 1 && todoID === todoList[0].id) {
+      //若刪除後 todoList 為 [], 希望把 id.current 清為 1
+      setTodoList([]);
+      id.current = 1;
+    } else {
+      //刪除 todoList 裡對應 id 的 todo 項目
+      setTodoList(todoList.filter((todo) => todoID !== todo.id));
+    }
+  };
   return (
     <Container>
       <TodoListWrapper>
@@ -53,7 +63,21 @@ function App() {
         <hr />
         <section>
           {todoList.length > 0 &&
-            todoList.map(({ todo, id }) => <div key={id}>{todo}</div>)}
+            todoList.map(({ todo, id }) => (
+              <section key={id}>
+                <div>{todo}</div>
+                <div>
+                  <input
+                    type="button"
+                    value="刪除"
+                    onClick={(e) => {
+                      e.preventDefault();
+                      handleDeleteOne(id);
+                    }}
+                  />
+                </div>
+              </section>
+            ))}
         </section>
       </TodoListWrapper>
     </Container>
